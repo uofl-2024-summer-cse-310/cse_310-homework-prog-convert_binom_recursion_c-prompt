afaecbaddd03e0bb19aa04c94a55f68b1cd998af
Nathan Russell <nathan@blackpathway.com>
2024-07-09 12:50:07 -0400

    first_try

--- binomial_coefficient.c
+++ binomial_coefficient.c
@@ -7,7 +7,10 @@ int factorial(int k) {
 }
 
 int binom(int k, int c) {
-    return factorial(k) / (factorial(c) * factorial(k - c));
+    if (c < 0 || k<0 || c>k) { return 0; }
+    if (c == 0 || c == k) { return 1; }
+    // return factorial(k) / (factorial(c) * factorial(k - c));
+    return binom(k - 1, c - 1) + binom(k - 1, c);
 }
 
 int main(int argc, char *argv[]) {
